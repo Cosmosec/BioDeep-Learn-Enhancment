c5c7feb59ec87675118fe0f653ebb5c7d13ed24a
Cosmosec <151138141+Cosmosec@users.noreply.github.com>
2020-06-24 15:57:55 +0000

    Release script commit.

--- ft_transformer.py
+++ ft_transformer.py
@@ -75,3 +75,195 @@ class GEGLU(nn.Module):
     """
 
     def forward(self, x: Tensor) -> Tensor:
+        return geglu(x)
+
+
+class _CLSToken(nn.Module):
+    """[CLS]-token for BERT-like inference.
+
+    To learn about the [CLS]-based inference, see [devlin2018bert].
+
+    When used as a module, the [CLS]-token is appended **to the end** of each item in
+    the batch.
+
+    Examples:
+        .. testcode::
+
+            batch_size = 2
+            n_tokens = 3
+            d_token = 4
+            cls_token = _CLSToken(d_token, 'uniform')
+            x = torch.randn(batch_size, n_tokens, d_token)
+            x = cls_token(x)
+            assert x.shape == (batch_size, n_tokens + 1, d_token)
+            assert (x[:, -1, :] == cls_token.expand(len(x))).all()
+
+    References:
+        * [devlin2018bert] Jacob Devlin, Ming-Wei Chang, Kenton Lee, Kristina Toutanova "BERT: Pre-training of Deep Bidirectional Transformers for Language Understanding" 2018
+    """
+
+    def __init__(self, d_token: int, initialization: str) -> None:
+        """
+        Args:
+            d_token: the size of token
+            initialization: initialization policy for parameters. Must be one of
+                :code:`['uniform', 'normal']`. Let :code:`s = d ** -0.5`. Then, the
+                corresponding distributions are :code:`Uniform(-s, s)` and :code:`Normal(0, s)`. In
+                the paper [gorishniy2021revisiting], the 'uniform' initialization was
+                used.
+
+        References:
+            * [gorishniy2021revisiting] Yury Gorishniy, Ivan Rubachev, Valentin Khrulkov, Artem Babenko "Revisiting Deep Learning Models for Tabular Data", 2021
+        """
+        super().__init__()
+        initialization_ = _TokenInitialization.from_str(initialization)
+        self.weight = nn.Parameter(Tensor(d_token))
+        initialization_.apply(self.weight, d_token)
+
+    def expand(self, *leading_dimensions: int) -> Tensor:
+        """Expand (repeat) the underlying [CLS]-token to a tensor with the given leading dimensions.
+
+        A possible use case is building a batch of [CLS]-tokens. See `_CLSToken` for
+        examples of usage.
+
+        Note:
+            Under the hood, the `torch.Tensor.expand` method is applied to the
+            underlying :code:`weight` parameter, so gradients will be propagated as
+            expected.
+
+        Args:
+            leading_dimensions: the additional new dimensions
+
+        Returns:
+            tensor of the shape :code:`(*leading_dimensions, len(self.weight))`
+        """
+        if not leading_dimensions:
+            return self.weight
+        new_dims = (1,) * (len(leading_dimensions) - 1)
+        return self.weight.view(*new_dims, -1).expand(*leading_dimensions, -1)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """Append self **to the end** of each item in the batch (see `_CLSToken`)."""
+        return torch.cat([x, self.expand(len(x), 1)], dim=1)
+
+
+class _TokenInitialization(enum.Enum):
+    UNIFORM = 'uniform'
+    NORMAL = 'normal'
+
+    @classmethod
+    def from_str(cls, initialization: str) -> '_TokenInitialization':
+        try:
+            return cls(initialization)
+        except ValueError:
+            valid_values = [x.value for x in _TokenInitialization]
+            raise ValueError(f'initialization must be one of {valid_values}')
+
+    def apply(self, x: Tensor, d: int) -> None:
+        d_sqrt_inv = 1 / math.sqrt(d)
+        if self == _TokenInitialization.UNIFORM:
+            # used in the paper "Revisiting Deep Learning Models for Tabular Data";
+            # is equivalent to `nn.init.kaiming_uniform_(x, a=math.sqrt(5))` (which is
+            # used by torch to initialize nn.Linear.weight, for example)
+            nn.init.uniform_(x, a=-d_sqrt_inv, b=d_sqrt_inv)
+        elif self == _TokenInitialization.NORMAL:
+            nn.init.normal_(x, std=d_sqrt_inv)
+
+
+class MultiheadAttention(nn.Module):
+    """Multihead Attention (self-/cross-) with optional 'linear' attention.
+
+    To learn more about Multihead Attention, see [devlin2018bert]. See the implementation
+    of `Transformer` and the examples below to learn how to use the compression technique
+    from [wang2020linformer] to speed up the module when the number of tokens is large.
+
+    Examples:
+        .. testcode::
+
+            n_objects, n_tokens, d_token = 2, 3, 12
+            n_heads = 6
+            a = torch.randn(n_objects, n_tokens, d_token)
+            b = torch.randn(n_objects, n_tokens * 2, d_token)
+            module = MultiheadAttention(
+                d_token=d_token, n_heads=n_heads, dropout=0.2, bias=True, initialization='kaiming'
+            )
+
+            # self-attention
+            x, attention_stats = module(a, a, None, None)
+            assert x.shape == a.shape
+            assert attention_stats['attention_probs'].shape == (n_objects * n_heads, n_tokens, n_tokens)
+            assert attention_stats['attention_logits'].shape == (n_objects * n_heads, n_tokens, n_tokens)
+
+            # cross-attention
+            assert module(a, b, None, None)
+
+            # Linformer self-attention with the 'headwise' sharing policy
+            k_compression = torch.nn.Linear(n_tokens, n_tokens // 4)
+            v_compression = torch.nn.Linear(n_tokens, n_tokens // 4)
+            assert module(a, a, k_compression, v_compression)
+
+            # Linformer self-attention with the 'key-value' sharing policy
+            kv_compression = torch.nn.Linear(n_tokens, n_tokens // 4)
+            assert module(a, a, kv_compression, kv_compression)
+
+    References:
+        * [devlin2018bert] Jacob Devlin, Ming-Wei Chang, Kenton Lee, Kristina Toutanova "BERT: Pre-training of Deep Bidirectional Transformers for Language Understanding" 2018
+        * [wang2020linformer] Sinong Wang, Belinda Z. Li, Madian Khabsa, Han Fang, Hao Ma "Linformer: Self-Attention with Linear Complexity", 2020
+    """
+
+    def __init__(
+        self,
+        *,
+        d_token: int,
+        n_heads: int,
+        dropout: float,
+        bias: bool,
+        initialization: str,
+    ) -> None:
+        """
+        Args:
+            d_token: the token size. Must be a multiple of :code:`n_heads`.
+            n_heads: the number of heads. If greater than 1, then the module will have
+                an addition output layer (so called "mixing" layer).
+            dropout: dropout rate for the attention map. The dropout is applied to
+                *probabilities* and do not affect logits.
+            bias: if `True`, then input (and output, if presented) layers also have bias.
+                `True` is a reasonable default choice.
+            initialization: initialization for input projection layers. Must be one of
+                :code:`['kaiming', 'xavier']`. `kaiming` is a reasonable default choice.
+        Raises:
+            AssertionError: if requirements for the inputs are not met.
+        """
+        super().__init__()
+        if n_heads > 1:
+            assert d_token % n_heads == 0, 'd_token must be a multiple of n_heads'
+        assert initialization in ['kaiming', 'xavier']
+
+        self.W_q = nn.Linear(d_token, d_token, bias)
+        self.W_k = nn.Linear(d_token, d_token, bias)
+        self.W_v = nn.Linear(d_token, d_token, bias)
+        self.W_out = nn.Linear(d_token, d_token, bias) if n_heads > 1 else None
+        self.n_heads = n_heads
+        self.dropout = nn.Dropout(dropout) if dropout else None
+
+        for m in [self.W_q, self.W_k, self.W_v]:
+            # the "xavier" branch tries to follow torch.nn.MultiheadAttention;
+            # the second condition checks if W_v plays the role of W_out; the latter one
+            # is initialized with Kaiming in torch
+            if initialization == 'xavier' and (
+                m is not self.W_v or self.W_out is not None
+            ):
+                # gain is needed since W_qkv is represented with 3 separate layers (it
+                # implies different fan_out)
+                nn.init.xavier_uniform_(m.weight, gain=1 / math.sqrt(2))
+            if m.bias is not None:
+                nn.init.zeros_(m.bias)
+        if self.W_out is not None:
+            nn.init.zeros_(self.W_out.bias)
+
+    def _reshape(self, x: Tensor) -> Tensor:
+        batch_size, n_tokens, d = x.shape
+        d_head = d // self.n_heads
+        return (
+            x.reshape(batch_size, n_tokens, self.n_heads, d_head)
+            .transpose(1, 2)
