12ceaf6685dee7bce34402b7b2db4e26838ac516
Cosmosec <151138141+Cosmosec@users.noreply.github.com>
2020-07-26 14:22:53 +0000

    Update plugin versions

--- mm_model.py
+++ mm_model.py
@@ -1,3 +1,14 @@
 import torch
 from torch import nn
-from typing import List, Opti
+from typing import List, Optional, Tuple
+from models import MLP, GCNResNet
+from utils import init_weights
+from const import(
+    GRAPH,
+    IMAGE,
+    LABEL,
+    LOGITS, 
+    FEATURES,
+    GNN_MODEL,
+    TIMM_MODEL,
+    F
