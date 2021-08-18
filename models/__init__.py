3cb732c77c6b08b8b36f4a96f2c693da54c95236
Cosmosec <151138141+Cosmosec@users.noreply.github.com>
2021-08-18 12:44:11 +0000

    Fixed compiler warnings

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,15 @@
 from .gnn import(
     GINTopK4,
- 
+    GINTopK2,
+    GCNTopK4,
+    GCNTopK2
+)
+
+from .resnet import(
+   resnet32,
+   resnet110,
+   wide_resnet20_8,
+)
+
+from .mlp import MLP 
+# from .
