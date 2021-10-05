b816619a02260b00c19241f1e3ff58c0314360dd
Cosmosec <151138141+Cosmosec@users.noreply.github.com>
2021-10-05 11:19:40 +0000

    qr code

--- models/resnet.py
+++ models/resnet.py
@@ -2,4 +2,6 @@
 ResNet for CIFAR-10/100 Dataset.
 
 Reference:
-1. https://github.com/
+1. https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py
+2. https://github.com/facebook/fb.resnet.torch/blob/master/models/resnet.lua
+3. Kaiming He
