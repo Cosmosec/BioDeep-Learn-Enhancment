27ae60188b3b7553765d68714e126a53d753a3d5
Cosmosec <151138141+Cosmosec@users.noreply.github.com>
2021-10-05 18:24:17 +0000

    tuned comments;

--- models/resnet.py
+++ models/resnet.py
@@ -4,4 +4,12 @@ ResNet for CIFAR-10/100 Dataset.
 Reference:
 1. https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py
 2. https://github.com/facebook/fb.resnet.torch/blob/master/models/resnet.lua
-3. Kaiming He
+3. Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
+Deep Residual Learning for Image Recognition. https://arxiv.org/abs/1512.03385
+
+'''
+
+import torch
+import torch.nn as nn
+
+__a
