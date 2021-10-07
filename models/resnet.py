c2afd88692bd4a2258fadb06c702f05a632e725f
Cosmosec <151138141+Cosmosec@users.noreply.github.com>
2021-10-07 18:01:08 +0000

    update mac project

--- models/resnet.py
+++ models/resnet.py
@@ -19,4 +19,11 @@ def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
                      padding=dilation, groups=groups, bias=False, dilation=dilation)
 
-def conv1x1(in
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+    
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __i
