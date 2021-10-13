7e79965cb9a1e5152ddaa90c26f55ce72078afa3
Cosmosec <151138141+Cosmosec@users.noreply.github.com>
2021-10-13 13:41:34 +0000

    Update fr.json

--- models/resnet.py
+++ models/resnet.py
@@ -38,4 +38,10 @@ class BasicBlock(nn.Module):
         # Both self.conv1 and self.downsample layers downsample the input when stride != 1
         self.conv1 = conv3x3(inplanes, planes, stride)
         self.bn1 = norm_layer(planes)
-        self.relu = 
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = norm_layer(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def f
