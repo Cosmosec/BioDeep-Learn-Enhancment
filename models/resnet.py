11ba2007247d81e790ffb2e5d4687fd567084f43
Cosmosec <151138141+Cosmosec@users.noreply.github.com>
2021-10-13 19:02:32 +0000

    Correzioni

--- models/resnet.py
+++ models/resnet.py
@@ -72,4 +72,6 @@ class Bottleneck(nn.Module):
         if norm_layer is None:
             norm_layer = nn.BatchNorm2d
         width = int(planes * (base_width / 64.)) * groups
-      
+        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv1x1(inplanes, width)
+        self.bn1 = norm_la
