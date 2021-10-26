b37f8e3b92a2585d75eed5ad88b950f2ee559513
Cosmosec <151138141+Cosmosec@users.noreply.github.com>
2021-10-26 08:12:15 +0000

    lolol

--- models/resnet.py
+++ models/resnet.py
@@ -77,4 +77,7 @@ class Bottleneck(nn.Module):
         self.bn1 = norm_layer(width)
         self.conv2 = conv3x3(width, width, stride, groups, dilation)
         self.bn2 = norm_layer(width)
-        self.conv3 = conv1x1(width, planes * se
+        self.conv3 = conv1x1(width, planes * self.expansion)
+        self.bn3 = norm_layer(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsamp
