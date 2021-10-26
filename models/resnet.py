c6f2c62540d21b96ae40f25f4d60dc29e3d4b65c
Cosmosec <151138141+Cosmosec@users.noreply.github.com>
2021-10-26 05:53:09 +0000

    Update readme.MD

--- models/resnet.py
+++ models/resnet.py
@@ -74,4 +74,7 @@ class Bottleneck(nn.Module):
         width = int(planes * (base_width / 64.)) * groups
         # Both self.conv2 and self.downsample layers downsample the input when stride != 1
         self.conv1 = conv1x1(inplanes, width)
-        self.bn1 = norm_la
+        self.bn1 = norm_layer(width)
+        self.conv2 = conv3x3(width, width, stride, groups, dilation)
+        self.bn2 = norm_layer(width)
+        self.conv3 = conv1x1(width, planes * se
