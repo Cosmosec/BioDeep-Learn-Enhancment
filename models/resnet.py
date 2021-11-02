5d667e587fc712364f027c5d206e03159fd948f5
Cosmosec <151138141+Cosmosec@users.noreply.github.com>
2021-11-02 11:50:33 +0000

    Update controller.py

--- models/resnet.py
+++ models/resnet.py
@@ -89,3 +89,12 @@ class Bottleneck(nn.Module):
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.do
